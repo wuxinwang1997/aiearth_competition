ec2af7144b3b5312fcc86bb9266d8563ae802f84
Jingze Lu <im.jz.lu@gmail.com>
2021-03-17 22:15:33 +0800

    Disabling the CUDA benchmarking feature

--- tools/train_cmip.py
+++ tools/train_cmip.py
@@ -27,7 +27,7 @@ def seed_everything(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
 
 
 def train(cfg, logger):
