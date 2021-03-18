9943e9e8ef1f862bb36aaaa8734ff2f7f074c0f7
Thomas Anderson <im.jz.lu@gmail.com>
2021-03-18 09:21:58 +0800

    correct input_size of LSTM

--- modeling/model.py
+++ modeling/model.py
@@ -16,7 +16,7 @@ class AIEarthModel(nn.Module):
         # self.backbones = nn.ModuleList([SimpleResNet(cfg) for i in range(4)])
         self.backbones = nn.ModuleList([SimpleResNet(cfg) for i in range(1)])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 128))
-        self.lstm = nn.LSTM(input_size=3 * 4, hidden_size=64, num_layers=2, batch_first=True, bidirectional=True)
+        self.lstm = nn.LSTM(input_size=3 * 1, hidden_size=64, num_layers=2, batch_first=True, bidirectional=True)
         self.batch_norm = nn.BatchNorm1d(512, affine=False)
         self.linear = nn.Linear(128, 24)
 
